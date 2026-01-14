d7a4d4a3c287b1ead2209d59f08536330ea76617
Lisbrow <l.brown91910@gmail.com>
2026-01-14 12:56:44 -0800

    Add fileURLToPath import and define __filename and __dirname in index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 
 import { pool } from "./db.js";
 import entries from "./routes/entries.js";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
